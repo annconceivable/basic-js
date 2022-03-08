12310224cc394a87c5b6c8be53a9844c089fd790
annconceivable <84342516+annconceivable@users.noreply.github.com>
2022-03-08 14:19:28 +0300

    init: solve hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,10 +14,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function calculateHanoi(discNumber, turnsSpeed) {
+  
+  //this is the recursive function that works but take a looot of time to execute with mupliple tests
+  
+  /*
+  function hanoi(discNumber) {
+    if (discNumber == 1) return 1;
+    else {
+      return ( hanoi(discNumber - 1) + 1 + hanoi(discNumber - 1));
+    }
+  }
+  */
+
+  // so I'll just use a formula 2**discNumber - 1 to find out the number of turns
+
+  let turns = Math.pow(2, discNumber) - 1;
+
+  let seconds = Math.floor( turns/(turnsSpeed/3600) );
+  
+  let res = {'turns': turns,'seconds': seconds};
+
+  return (res);
 }
+
 
 module.exports = {
   calculateHanoi
