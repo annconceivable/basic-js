e5184da4286d2853f07006c5ff5e3992c290d49a
annconceivable <84342516+annconceivable@users.noreply.github.com>
2022-03-08 14:22:59 +0300

    init: solve transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if (!( Array.isArray(arr) )) throw new Error("'arr' parameter must be an instance of the Array!");
+		let res = arr.slice();
+		let del = 0;
+		let cmd = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+		for (let i = 0; i < res.length; i++) {
+			if ( ( i == 0 )&&( ( res[i] == cmd[1]) | ( res[i] == cmd[3] ) ) ) {
+				res.splice(0, 1);
+			}
+
+			if ( ( i == (res.length - 1)) && ( (res[i] == cmd[0]) | (res[i] == cmd[2] ) ) ) {
+				res.splice((res.length-1), 1);
+			}
+
+
+			if ( res[i] == cmd[0] ) {
+				res.splice(i, 2);
+				if ( (res[i] == cmd[1]) | (res[i] == cmd[3]) ) {
+					res.splice(i, 1);
+				}
+			}
+			if ( res[i] == cmd[1] ){
+				res.splice(i-1, 2);
+			}
+			if ( res[i] == cmd[2] ){
+				res.splice(i, 1, res[i+1]);
+			}
+			if ( res[i] == cmd[3] ) {
+				res.splice(i, 1, res[i-1]);
+			}
+		}
+		return res;
 }
 
 module.exports = {
