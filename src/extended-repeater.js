b637de27da4447eb64eee864e50e9c70ce369291
annconceivable <84342516+annconceivable@users.noreply.github.com>
2022-05-02 11:35:30 +0300

    Recursive depth calculator

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let string = String(str);
+  let addition = 
+    typeof options.addition === "boolean" 
+    || options.addition === null 
+    || options.addition 
+      ? String(options.addition) 
+      : '';
+  let repeatTimes = options.repeatTimes || 1;
+  let additionRepeatTimes = options.additionRepeatTimes || 1;
+  let separator = "";
+  if (options.separator) separator = String(options.separator);
+  else separator = "+";
+  let additionSeparator = "";
+  if (options.additionSeparator) additionSeparator = String(options.additionSeparator);
+  else additionSeparator = "|";
+
+  let res = "";
+
+  for (let i = 0; i < repeatTimes; i++) {
+    res += string;
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      res += addition;
+      if (j + 1 < additionRepeatTimes) {
+        res += additionSeparator;
+      } 
+    }
+    if (i + 1 < repeatTimes) {
+      res += separator;
+    }
+  }
+  return res;
 }
 
 module.exports = {
