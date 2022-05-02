f05c59fe71629b73452bbc8f9b52ec96632dd62c
annconceivable <84342516+annconceivable@users.noreply.github.com>
2022-05-02 20:48:58 +0300

    vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,84 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if ( message === undefined || message === null || message === '' || key === undefined || key === null || key === '') throw new Error('Incorrect arguments!');
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let encrypted = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    if (message.length > key.length) {
+      for (let i = 0, j = 0; i < message.length; i++) {
+        if (!alphabet.includes(message[i])){
+          encrypted += message[i];
+          continue;
+        }  
+        if (j < key.length) {
+          encrypted += alphabet[ (alphabet.indexOf(message[i]) + alphabet.indexOf(key[j])) % 26 ];
+          
+          j++;
+        }
+        else {
+          j = 0;
+          encrypted += alphabet[ (alphabet.indexOf(message[i]) + alphabet.indexOf(key[j])) % 26 ];
+          j++;
+        }
+      }
+    }
+    if (message.length <= key.length) {
+      for (let i = 0; i < message.length; i++) {
+        if (!alphabet.includes(message[i])){
+          encrypted += message[i];
+          continue;
+        }  
+        encrypted += alphabet[ (alphabet.indexOf(message[i]) + alphabet.indexOf(key[i])) % 26 ];
+      }
+    }
+
+    return this.mode ? encrypted : encrypted.split('').reverse().join('');
+  }
+  decrypt(encryptedMessage, key1) {
+    if (encryptedMessage === undefined || encryptedMessage === null || encryptedMessage === '' || key1 === undefined || key1 === null || key1 === '') throw new Error('Incorrect arguments!');
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let decrypted = '';
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key1 = key1.toUpperCase();
+
+    if (encryptedMessage.length > key1.length) {
+      
+      for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+        if (!alphabet.includes(encryptedMessage[i])){
+          decrypted += encryptedMessage[i];
+          continue;
+        }  
+        if (j < key1.length) {
+          decrypted += alphabet[ (alphabet.indexOf(encryptedMessage[i]) + 26 - alphabet.indexOf(key1[j])) % 26 ];
+          
+          j++;
+        }
+        else {
+          j = 0;
+          decrypted += alphabet[ (alphabet.indexOf(encryptedMessage[i]) + 26 - alphabet.indexOf(key1[j])) % 26 ];
+          j++;
+        }
+      }
+    }
+    if (encryptedMessage.length <= key1.length) {
+      for (let i = 0; i < encryptedMessage.length; i++) {
+        if (!alphabet.includes(encryptedMessage[i])){
+          decrypted += encryptedMessage[i];
+          continue;
+        }  
+        decrypted += alphabet[ (alphabet.indexOf(encryptedMessage[i]) + 26 - alphabet.indexOf(key1[i])) % 26 ];
+      }
+    }
+
+    return this.mode ? decrypted : decrypted.split('').reverse().join('');
+   
   }
 }
 
